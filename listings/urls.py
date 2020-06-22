361b09a2077d3a9b4032d2f4bb6bc38f2d43d596
Jackie Santana <jackiebrown.nyc@gmail.comgit config --global user.email jackiebrown.nyc@gmail.com>
2020-06-22 15:54:01 -0500

    done

--- listings/urls.py
+++ listings/urls.py
@@ -1,9 +1,9 @@
-from django.urls import path 
+from django.urls import path
 
 from . import views
 
 urlpatterns = [
     path('', views.index, name='listings'),
     path('<int:listing_id>', views.listing, name='listing'),
-    path('search', views.search, name='search')
+    path('search', views.search, name='search'),
 ]
--- pages/urls.py
+++ pages/urls.py
@@ -1,8 +1,8 @@
-from django.urls import path 
+from django.urls import path
 
 from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('about', views.about, name='about')
+    path('about', views.about, name='about'),
 ]
